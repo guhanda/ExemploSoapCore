63b5a08280d1579308e1e5b6672f6f732e242561
Handa <guhanda@yahoo.com.br>
2018-09-27 02:20:16 -0300

    Primeiro commit

---
+++ WebApplication1/PingService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Threading.Tasks;
+
+namespace WebApplication1
+{
+    [ServiceContract]
+    public interface IPingService
+    {
+        [OperationContract]
+        string Ping(string msg);
+    }
+
+    public class PingService : IPingService
+    {
+        public string Ping(string msg)
+        {
+            return msg;
+        }
+    }
+}
---
+++ WebApplication1/Startup.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using SoapCore;
+
+namespace WebApplication1
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.TryAddSingleton<PingService>();
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+
+            app.UseSoapEndpoint<PingService>("/Service.asmx", new BasicHttpBinding());
+            app.UseMvc();
+            
+        }
+    }
+}
